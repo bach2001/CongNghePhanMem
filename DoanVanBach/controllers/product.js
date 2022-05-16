be237d3d8e39e18fdd8001660376cf758ed91159
bachcool104@gmail.com <bachcool104@gmail.com>
2022-05-16 14:36:17 +0700

    controler-product

---
+++ DoanVanBach/controllers/product.js
@@ -0,0 +1,31 @@
+const productModel = require("../models/products");
+const fs = require("fs");
+const path = require("path");
+
+class Product {
+  // Delete Image from uploads -> products folder
+  static deleteImages(images, mode) {
+    var basePath =
+      path.resolve(__dirname + "../../") + "/public/uploads/products/";
+    console.log(basePath);
+    for (var i = 0; i < images.length; i++) {
+      let filePath = "";
+      if (mode == "file") {
+        filePath = basePath + `${images[i].filename}`;
+      } else {
+        filePath = basePath + `${images[i]}`;
+      }
+      console.log(filePath);
+      if (fs.existsSync(filePath)) {
+        console.log("Exists image");
+      }
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          return err;
+        }
+      });
+    }
+  }
+
+//...
+}
